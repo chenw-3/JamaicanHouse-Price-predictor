96ececac57091a53852e67305ba4e3d6248f3821
Chennel Williams <williamschennel@gmail.com>
2021-08-01 09:07:04 -0500

    trying to load pickle file

--- notebooks/first_app.py
+++ notebooks/first_app.py
@@ -5,7 +5,7 @@ import pickle
 import string
 
 #load model:
-model = pickle.load(open('\price_model.pkl', 'rb'))
+model = pickle.load(open('notebooks\price_model.pkl', 'rb'))
 
 def main():
     # Title
